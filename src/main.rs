98cb4bdd88e7500f2b7a41701ab7888c03137133
Zane Littrell <littrellzane@gmail.com>
2024-02-05 01:36:31 +0000

    Add player and initial movement code.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use cursive::event::{Event, EventResult};
 use cursive::views::Canvas;
 use cursive::{Cursive, CursiveExt, XY};
 
@@ -12,13 +13,16 @@ enum Tile {
 
 struct GameState {
     tiles: [Tile; MAP_W * MAP_H],
+    player_location: XY<usize>,
 }
 
 impl GameState {
     fn new() -> Self {
         let mut state = Self {
             tiles: [Tile::Blank; MAP_W * MAP_H],
+            player_location: XY::new(MAP_W / 2, MAP_H / 2),
         };
+        // Setup walls around the perimeter
         for y in 0..MAP_H {
             for x in 0..MAP_W {
                 if y == 0 || y == MAP_H - 1 {
@@ -47,6 +51,7 @@ fn create_canvas() -> Canvas<GameState> {
             XY::new(MAP_W, MAP_H)
         })
         .with_draw(|state: &GameState, printer| {
+            // Draw tiles first
             for y in 0..MAP_H {
                 for x in 0..MAP_W {
                     let tile = state.tiles[y * MAP_W + x];
@@ -56,5 +61,23 @@ fn create_canvas() -> Canvas<GameState> {
                     }
                 }
             }
+            // Draw player on top
+            printer.print(state.player_location, "@");
+        })
+        .with_on_event(|state: &mut GameState, event| match event {
+            Event::WindowResize => todo!(),
+            Event::Char(k) => {
+                if k == 'w' {
+                    state.player_location.y -= 1;
+                } else if k == 's' {
+                    state.player_location.y += 1;
+                } else if k == 'a' {
+                    state.player_location.x -= 1;
+                } else if k == 'd' {
+                    state.player_location.x += 1;
+                }
+                EventResult::Ignored
+            }
+            _ => todo!(),
         })
 }
